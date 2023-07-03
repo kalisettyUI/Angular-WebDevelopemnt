8ba940b105418bc0b4d68f94a62c1c7e8421df39
anilkumarmudum <your_email@example.com>
2023-07-03 19:23:17 +0530

    develop

---
+++ jothi.js
@@ -0,0 +1,29 @@
+var greeter = "good morning";
+function myfirstfunction() {
+    var wishes = "Hey Hi";
+    console.log(wishes);
+    console.log(greeter);
+}
+myfirstfunction();
+console.log(wishes);
+console.log(greeter);
+    
+
+var
+var greetings = "hi ";
+console.log(greetings);
+var greetings = "hello";
+console.log(greetings);
+greetings = "water";
+console.log(greetings);
+ let
+
+let cityName = "pileru";
+console.log(cityName);
+cityName = "tirupathi";
+console.log(cityName);
+const
+const ganga = "ocean";
+console.log(ganga);
+ganga = "river";
+console.log(ganga);
